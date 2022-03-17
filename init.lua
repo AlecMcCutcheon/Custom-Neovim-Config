8503a628739efbacdddebc2043f9b73fd3f9ef42
Alec McCutcheon <alec@obc.tech>
2022-03-16 21:46:48 -0400

    theming

--- init.lua
+++ init.lua
@@ -20,5 +20,7 @@ require "user.alpha"
 require "user.whichkey"
 require "user.autocommands"
 require "user.colorizer"
+vim.cmd [[ hi statusline guibg=NONE gui=NONE ]]
+vim.cmd [[ set fillchars=fold:\ ,vert:\│,eob:\ ,msgsep:‾ ]]
 vim.cmd [[ let g:instant_username = "MixtapeXD" ]]
 vim.cmd [[ let g:coc_global_extensions=[ 'coc-powershell', 'coc-snippets' ] ]]
